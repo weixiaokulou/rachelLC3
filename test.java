773485d98f267e4c8f22528bb7abcf9bfa4c40df
jjtpc1205 <837492084@qq.com>
2016-03-27 23:10:09 -0400

    Add sim consle
    Add sim consle

--- test.java
+++ test.java
@@ -1,7 +1,9 @@
 import java.io.*;
 
 public class test {
-    public static void main(String [] args) {
+    public static void main(String [] args) throws IOException {
+    	
+    	new JF();
         String fileName = "1.txt";
         CPU cpu=new CPU();
         int entry=0x30;
@@ -25,7 +27,7 @@ public class test {
             while(true) {
               int mar = cpu.PC;
               if(cpu.memory[mar] == 0x0 ) break;
-              System.out.println("——————————————————————————");
+              System.out.println("----------------------------------------------------------------------------");
               System.out.println("Fetch finish, the instruction is: "+intToString(cpu.memory[mar], 4));
               cpu.PC = cpu.PC + 1;
               cpu.Decode(cpu.memory[mar],cpu);    
